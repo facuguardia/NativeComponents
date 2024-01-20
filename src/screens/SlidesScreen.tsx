1079735a0046081dded756f82223c9d5b7426010
Facundo Guardia <101948163+facuguardia@users.noreply.github.com>
2024-01-20 12:16:52 -0300

    Add slides screen and assets

---
+++ src/screens/SlidesScreen.tsx
@@ -0,0 +1,146 @@
+import React, {useState} from 'react';
+import {
+  View,
+  SafeAreaView,
+  ImageSourcePropType,
+  Image,
+  Dimensions,
+  Text,
+  StyleSheet,
+  Animated,
+} from 'react-native';
+import Carousel, {Pagination} from 'react-native-snap-carousel';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+import Icon from 'react-native-vector-icons/Ionicons';
+import {useAnimation} from '../hooks/useAnimation';
+import {useNavigation} from '@react-navigation/native';
+import {StackScreenProps} from '@react-navigation/stack';
+
+const {height, width} = Dimensions.get('window');
+
+interface Slide {
+  title: string;
+  desc: string;
+  img: ImageSourcePropType;
+}
+
+const items: Slide[] = [
+  {
+    title: 'Titulo 1',
+    desc: 'Ea et eu enim fugiat sunt reprehenderit sunt aute quis tempor ipsum cupidatat et.',
+    img: require('../assets/slide-1.png'),
+  },
+  {
+    title: 'Titulo 2',
+    desc: 'Anim est quis elit proident magna quis cupidatat culpa labore Lorem ea. Exercitation mollit velit in aliquip tempor occaecat dolor minim amet dolor enim cillum excepteur. ',
+    img: require('../assets/slide-2.png'),
+  },
+  {
+    title: 'Titulo 3',
+    desc: 'Ex amet duis amet nulla. Aliquip ea Lorem ea culpa consequat proident. Nulla tempor esse ad tempor sit amet Lorem. Velit ea labore aute pariatur commodo duis veniam enim.',
+    img: require('../assets/slide-3.png'),
+  },
+];
+
+interface Props extends StackScreenProps<any, any> {}
+
+export const SlidesScreen = ({navigation}: Props) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [isVisible, setIsVisible] = useState(false);
+  const {opacity, fadeIn} = useAnimation();
+
+  const renderItem = (item: Slide) => {
+    return (
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: 'white',
+          borderRadius: 5,
+          justifyContent: 'center',
+        }}>
+        <Image
+          source={item.img}
+          style={{width: 350, height: 400, resizeMode: 'center'}}
+        />
+        <View style={styles.container}>
+          <Text style={styles.title}>{item.title}</Text>
+          <Text style={styles.description}>{item.desc}</Text>
+        </View>
+      </View>
+    );
+  };
+
+  return (
+    <SafeAreaView style={{flex: 1, paddingTop: 50}}>
+      <Carousel
+        data={items}
+        renderItem={({item}: any) => renderItem(item)}
+        sliderWidth={width}
+        itemWidth={width}
+        layout="default"
+        onSnapToItem={index => {
+          setActiveIndex(index);
+          if (index === 2) {
+            setIsVisible(true);
+            fadeIn();
+          }
+        }}
+      />
+
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: isVisible ? 'space-between' : 'center',
+          alignItems: 'center',
+          paddingHorizontal: 20,
+        }}>
+        <Pagination
+          dotsLength={items.length}
+          activeDotIndex={activeIndex}
+          dotStyle={{
+            width: 10,
+            height: 10,
+            borderRadius: 10,
+            backgroundColor: '#5856D6',
+          }}
+        />
+
+        {isVisible && (
+          <Animated.View style={{opacity}}>
+            <TouchableOpacity
+              style={{
+                flexDirection: 'row',
+                backgroundColor: '#5856D6',
+                width: 140,
+                height: 50,
+                borderRadius: 10,
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
+              activeOpacity={0.8}
+              onPress={() => {navigation.navigate('HomeScreen')}}>
+              <Text style={{color: 'white', fontSize: 25}}>Entrar</Text>
+              <Icon name="chevron-forward-outline" size={30} color="white" />
+            </TouchableOpacity>
+          </Animated.View>
+        )}
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 30,
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: '#5856D6',
+  },
+  description: {
+    fontSize: 16,
+    fontWeight: '500',
+  },
+});
